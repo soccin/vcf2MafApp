581adfe0524b1db9f8a4ea922ba6127c7fe66eb6
Nicholas D. Socci <soccin@mskcc.org>
2024-04-20 14:41:46 -0400

    Added b37 genome

--- vcf2maf.sh
+++ vcf2maf.sh
@@ -4,23 +4,32 @@ module load samtools/1.19.2
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-MROOT=$(realpath $SDIR/../resources)
+GENOME=$1
+IVCF=$2
+NID=$3
+TID=$4
+
+
+MROOT=$(realpath $SDIR/rsrc)
 VEPROOT=$MROOT/vep
 VEPDATA=$MROOT/vep/vep_data
 
-VCF2MAF=$MROOT/vcf2maf-1.6.21/vcf2maf.pl
+VCF2MAF=$MROOT/vcf2maf/vcf2maf-1.6.21/vcf2maf.pl
 
-IVCF=$1
-NID=$2
-TID=$3
+if [ -e $GENOME ]; then
+    source $GENOME
+else
+    echo -e "\n   Invalid genome file [$GENOME]\n\n"
+    exit
+fi
 
 perl $VCF2MAF \
     --vep-path $SDIR \
     --vep-data $VEPDATA \
     --vep-forks 12  \
-    --ref-fasta $VEPDATA/mus_musculus/102_GRCm38/Mus_musculus.GRCm38.dna.toplevel.fa.gz \
-    --species mus_musculus \
-    --ncbi-build GRCm38 \
+    --ref-fasta $FASTA \
+    --species $SPECIES \
+    --ncbi-build $BUILD \
     --retain-info CALLER,DP,STATUS \
     --retain-fmt AD \
     --input-vcf $IVCF \
