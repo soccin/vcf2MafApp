4599672f062b1f7c1efc6f6a1a53521c45eb416e
Nicholas D. Socci <soccin@mskcc.org>
2024-08-15 18:18:39 -0400

    Added 1 sample version

---
+++ vcf2maf1samp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+module load samtools/1.19.2
+
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+GENOME=$1
+IVCF=$2
+SID=$3
+
+
+MROOT=$(realpath $SDIR/rsrc)
+VEPROOT=$MROOT/vep
+VEPDATA=$MROOT/vep/vep_data
+
+VCF2MAF=$MROOT/vcf2maf/vcf2maf-1.6.21/vcf2maf.pl
+
+if [ -e $SDIR/${GENOME}.sh ]; then
+    source $SDIR/${GENOME}.sh
+else
+    echo -e "\n   Invalid genome file [$GENOME]\n\n"
+    exit
+fi
+
+perl $VCF2MAF \
+    --vep-path $SDIR \
+    --vep-data $VEPDATA \
+    --vep-forks 12  \
+    --ref-fasta $FASTA \
+    --species $SPECIES \
+    --ncbi-build $BUILD \
+    --retain-info CALLER,DP,STATUS \
+    --retain-fmt AD \
+    --input-vcf $IVCF \
+    --output-maf ${IVCF/.vcf/.vep.maf} \
+    --tumor-id $SID
